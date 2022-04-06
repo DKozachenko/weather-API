421d20e047c388a12c5e1b765647a3b19ed04204
Diana <kozachenkodiana03@mail.ru>
2022-04-06 17:01:30 +0700

    Added base template

--- weather-api/src/App.vue
+++ weather-api/src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>
-    <h1>Hello world</h1>
-  </div>
+  <main>
+    <div class="container">
+      <div class="row mb-3">
+        <div class="col-12 text-center">
+          <h1>Whether API Service</h1>
+        </div>
+      </div>
+
+      <div class="row mb-3 d-flex justify-content-center">
+        <div class="col-6">
+          <div class="input-group">
+            <span class="input-group-text" id="basic-addon1">City: </span>
+            <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+          </div>
+        </div>
+      </div>
+
+      <weather-view :weather-object="weatherObj1"></weather-view>
+    </div>
+
+  </main>
 </template>
 
 <script>
+import WeatherView from "@/components/WeatherView";
 export default {
-
+  components: {WeatherView},
+  data() {
+    return {
+      isLoading1Api: false,
+      weatherObj1: {
+        currentWeather: 45,
+        veter: '45 cvfss'
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  main {
+    padding: 40px 0;
+    height: 100%;
+    min-height: 100vh;
+    background: lightsalmon;
+  }
 </style>
---
+++ weather-api/src/components/WeatherView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="weather w-50 h-50 border rounded-1 border-primary"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    weatherObject: Object
+  }
+}
+</script>
+
+<style scoped>
+  .weather {
+    width: 300px;
+    height: 150px;
+  }
+</style>
