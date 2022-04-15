e381a61126eed56c49da15d0827fafb7b6227c6c
Diana <kozachenkodiana03@mail.ru>
2022-04-15 15:29:39 +0700

    Stylized weather box, process some data

--- weather-api/src/App.vue
+++ weather-api/src/App.vue
@@ -18,12 +18,17 @@
         </div>
       </div>
 
-      <div v-if="isLoading1ApiWeather || isLoading1ApiOpenWeather" class="row">
+      <div v-if="isError" class="row">
+        <div class="col-12 text-center">
+          <h3>There is no this town in town database</h3>
+        </div>
+      </div>
+      <div v-else-if="!isError && (isLoading1ApiWeather || isLoading1ApiOpenWeather)" class="row">
         <div class="col-12 text-center">
           <h3>Loading</h3>
         </div>
       </div>
-      <div v-else class="row">
+      <div v-else-if="isFirstSearch" class="row">
         <div class="col-4">
           <weather-view :weather-object="weatherObjOpenWeather"></weather-view>
         </div>
@@ -51,6 +56,8 @@ export default {
       cityName: 'London',
       isLoading1ApiOpenWeather: false,
       isLoading1ApiWeather: false,
+      isError: false,
+      isFirstSearch: false,
       weatherObjOpenWeather: {
         temp: '',
         feelsLike: '',
@@ -58,7 +65,9 @@ export default {
         pressure: '',
         windDirection: '',
         windSpeed: '',
-        icon: ''
+        icon: '',
+        fromName: 'Open Weather',
+        fromLink: 'https://openweathermap.org/'
       },
       weatherObjWeather: {
         temp: '',
@@ -67,14 +76,20 @@ export default {
         pressure: '',
         windDirection: '',
         windSpeed: '',
-        icon: ''
+        icon: '',
+        fromName: 'Weather API',
+        fromLink: 'https://www.weatherapi.com/'
       },
     }
   },
   methods: {
     async getApiOpenWeather() {
-      this.isLoading1ApiOpenWeather = true
-      return await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=${this.API_KEY_OPENWEATHER}&units=metric`)
+      try {
+        this.isLoading1ApiOpenWeather = true
+        return await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=${this.API_KEY_OPENWEATHER}&units=metric`)
+      } catch (e) {
+        this.isError = true
+      }
     },
     async getApiGismeteo() {
       return await axios.get(`https://api.gismeteo.net/v2/weather/current/?latitude=54.35&longitude=52.52`, {
@@ -95,46 +110,110 @@ export default {
       })
     },
     async getApiWeather() {
-      this.isLoading1ApiWeather = true
-      return await axios.get(`http://api.weatherapi.com/v1/current.json?key=${this.API_KEY_WEATHER}&q=${this.cityName}&aqi=no`)
+      try {
+        this.isLoading1ApiWeather = true
+        return await axios.get(`http://api.weatherapi.com/v1/current.json?key=${this.API_KEY_WEATHER}&q=${this.cityName}&aqi=no`)
+      } catch (e) {
+        this.isError = true
+      }
+
     },
     fillOpenWeatherObj(response) {
-
       this.weatherObjOpenWeather = {
-        temp: response.data.main.temp,
-        feelsLike: response.data.main.feels_like,
+        ...this.weatherObjOpenWeather,
+        temp: response.data.main.temp.toFixed(0),
+        feelsLike: response.data.main.feels_like.toFixed(0),
         description: response.data.weather[0].description[0].toUpperCase() + response.data.weather[0].description.slice(1),
         pressure: (response.data.main.pressure * 0.750062).toFixed(0) ,
-        windDirection: response.data.wind.deg.toString(),
+        windDirection: this.processWindDirection(response.data.wind.deg),
         windSpeed: response.data.wind.speed.toString(),
         icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
       }
     },
     fillWeatherObj(response) {
       this.weatherObjWeather = {
-        temp: response.data.current.temp_c,
-        feelsLike: response.data.current.feelslike_c,
+        ...this.weatherObjWeather,
+        temp: response.data.current.temp_c.toFixed(0),
+        feelsLike: response.data.current.feelslike_c.toFixed(0),
         description: response.data.current.condition.text,
         pressure: (response.data.current.pressure_mb * 0.750062).toFixed(0),
-        windDirection: response.data.current.wind_degree.toString(),
+        windDirection: this.processWindDirection(response.data.current.wind_degree),
         windSpeed: (response.data.current.wind_mph * 5 / 18).toFixed(2).toString(),
         icon: response.data.current.condition.icon
+      }
+    },
+    processWindDirection(deg) {
+      if (deg > 348 && deg <= 360 || deg >= 0 && deg <= 11) {
+        return "N"
+      }
+      if (deg > 11 && deg <= 33) {
+        return "NNE"
+      }
+      if (deg > 33 && deg <= 56) {
+        return "NE"
+      }
+      if (deg > 56 && deg <= 78) {
+        return "ENE"
+      }
+      if (deg > 78 && deg <= 101) {
+        return "E"
+      }
+      if (deg > 101 && deg <= 123) {
+        return "ESE"
+      }
+      if (deg > 123 && deg <= 146) {
+        return "SE"
+      }
+      if (deg > 146 && deg <= 168) {
+        return "SSE"
+      }
+      if (deg > 168 && deg <= 191) {
+        return "S"
+      }
+      if (deg > 191 && deg <= 213) {
+        return "SSW"
+      }
+      if (deg > 213 && deg <= 236) {
+        return "SW"
+      }
+      if (deg > 236 && deg <= 258) {
+        return "WSW"
+      }
+      if (deg > 258 && deg <= 281) {
+        return "W"
+      }
+      if (deg > 281 && deg <= 303) {
+        return "WNW"
+      }
+      if (deg > 303 && deg <= 326) {
+        return "NW"
+      }
+      if (deg > 326 && deg <= 348) {
+        return "NNW"
       }
     },
     //по долготе и широте
     async getAllData() {
       const forecast1 = await this.getApiOpenWeather()
-      this.isLoading1ApiOpenWeather = false
-      this.fillOpenWeatherObj(forecast1)
-      console.log(this.weatherObjOpenWeather)
+      if (!this.isError) {
+        this.isLoading1ApiOpenWeather = false
+        this.isError = false
+        this.isFirstSearch = true
+        this.fillOpenWeatherObj(forecast1)
+      }
+
       //const forecast2 = await this.getApiYandex()
       //console.log(forecast2)
       //const forecast3 = await this.getApi3()
       //console.log(forecast3)
       const forecast4 = await this.getApiWeather()
-      this.isLoading1ApiWeather = false
-      this.fillWeatherObj(forecast4)
-      console.log(this.weatherObjWeather)
+      if (!this.isError) {
+        this.isLoading1ApiWeather = false
+        this.isError = false
+        this.isFirstSearch = true
+        this.fillWeatherObj(forecast4)
+      }
+
     }
   },
 }
--- weather-api/src/components/WeatherView.vue
+++ weather-api/src/components/WeatherView.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="row">
     <div class="col-12">
-      <div class="weather border rounded-1 border-primary">
-        <p>{{ weatherObject.temp }}</p>
-        <p>{{ weatherObject.feelsLike }}</p>
-        <p>{{ weatherObject.description }}</p>
-        <p>{{ weatherObject.pressure }}</p>
-        <p>{{ weatherObject.windDirection }}</p>
-        <p>{{ weatherObject.windSpeed }}</p>
-        <img :src="weatherObject.icon" alt="icon">
+      <div class="weather border rounded rounded-3 border-dark py-2 px-4">
+        <div class="top">
+          <div class="top__img">
+            <img :src="weatherObject.icon" alt="icon">
+          </div>
+          <p>{{ weatherObject.temp > 0 ? '+' : '' }}{{ weatherObject.temp }}</p>
+          <p>{{ weatherObject.feelsLike > 0 ? '+' : '' }}{{ weatherObject.feelsLike }}<br> По ощущениям </p>
+        </div>
+
+        <div class="middle">
+          <div class="middle__left">
+            <p>{{ weatherObject.description }}</p>
+            <p>{{ weatherObject.pressure }} мм.рт.ст.</p>
+          </div>
+          <div class="middle__right">
+            <p>{{ weatherObject.windDirection }}</p>
+            <p>{{ weatherObject.windSpeed }} м/с</p>
+          </div>
+        </div>
+
+        <div class="down">
+          <a v-bind:href="weatherObject.fromLink">{{ weatherObject.fromName }}</a>
+        </div>
       </div>
     </div>
   </div>
@@ -23,9 +38,50 @@ export default {
 </script>
 
 <style scoped>
-  .weather {
-    width: 300px;
-    height: 150px;
-    border: 1px solid black;
+  .top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-bottom: 15px;
+  }
+
+  .top__img {
+    width: 55px;
+  }
+
+  .top__img img {
+    width: 100%;
+  }
+
+  .top p {
+    font-size: 25px;
+    margin-bottom: 0;
+  }
+
+  .top p:last-child {
+    font-size: 14px;
+  }
+
+  .middle {
+    display: flex;
+    justify-content: space-between;
+
+    margin-bottom: 15px;
+  }
+
+  .middle__left,
+  .middle__right {
+    flex-basis: 45%;
+  }
+
+  .middle__left p,
+  .middle__right p {
+    margin-bottom: 8px;
+  }
+
+  .down {
+    display: flex;
+    justify-content: flex-end;
   }
 </style>
